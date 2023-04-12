8eb548a879ee0fc3fa1e9850c8b340fb01bb65bb
helderzack <helderzacarias@outlook.com>
2023-04-12 14:57:28 -0300

    init: mobile design of the first form is finished

--- README.md
+++ README.md
@@ -1,38 +1,47 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Frontend Mentor - Multi-step form
 
-## Getting Started
+This is my solution to the [Multi-step form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/multistep-form-YVAnSdqQBJ). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
-First, run the development server:
+## Table of contents
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+- [Overview](#overview)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- [Links](#links)
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+- [My process](#my-process)
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+- [Built with](#built-with)
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+- [What I learned](#what-i-learned)
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+- [Continued development](#continued-development)
 
-## Learn More
+- [Useful resources](#useful-resources)
 
-To learn more about Next.js, take a look at the following resources:
+- [Author](#author)
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## Overview
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+### Links
+  
+- Solution URL: [GitHub Repository](https://github.com/helderzack/frontend-mentor_multi-step-form.git)
 
-## Deploy on Vercel
+## My process
+  
+### Built with
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+- Next.js
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- Flexbox
+
+### What I learned
+
+I'm trying to use Next.js, for this project, to get familiarized with how the framework works.
+
+### Continued development
+
+II still have to develop the javascript code to make the site dynamic.
+
+## Author
+
+- Frontend Mentor - [@helderzack](https://www.frontendmentor.io/profile/helderzack)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,28 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className={styles['card']}>
+      <div className={styles['card__content']}>
+        <div className={styles['card__text']}>
+          <p className={styles['card__title']}>Personal info</p>
+          <p className={styles['card__description']}>Please provide your name, email address, and phone number.</p>
         </div>
+        <form className={styles['info-form']}>
+          <div className={styles['input-container']}>
+            <label htmlFor="name" className={styles['input-container__label']}>Name</label>
+            <input type="text" name="name" id="name" placeholder='e.g. Stephen King' className={styles['input-container__input']} />
+          </div>
+          <div className={styles['input-container']}>
+            <label htmlFor="email" className={styles['input-container__label']}>Email</label>
+            <input type="email" name="email" id="email" placeholder='e.g. stephenking@lorem.com' className={styles['input-container__input']} />
+          </div>
+          <div className={styles['input-container']}>
+            <label htmlFor="phone" className={styles['input-container__label']}>Phone Number</label>
+            <input type="text" name="phone" placeholder='e.g. +1 234 567 890' id="phone" className={styles['input-container__input']} />
+          </div>
+        </form>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section >
   )
 }
